a8ecd029c0a54b8a4c25c405857c548879e9f5d7
HayHay <dev@hayhay.dev>
2023-06-21 19:51:34 -0400

    Create README and env

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ config({});
 const app = express();
 app.use(express.json());
 const wsInstance = expressWs(app);
-const port = 3000;
+const port = process.env.PORT || 3000;
 const commandMap = new CommandMap();
 const tiltify = new TiltifyAPI();
 
