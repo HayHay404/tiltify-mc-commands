4210329f5486bccbe5672b0db9431fcb502ffbbf
HayHay <dev@hayhay.dev>
2023-06-21 18:53:57 -0400

    Update examples and custom commands

--- src/utils/extra/CustomCommands.ts
+++ src/utils/extra/CustomCommands.ts
@@ -43,4 +43,52 @@ export default class CustomCommands {
 
     return ["/say Summoned some farm animals!", ...summonCommands];
   }
+
+  randomSummon() {
+    const entityList = [
+      "lightning_bolt",
+      "creeper ~ ~ ~ {powered:3}",
+      "warden",
+    ];
+
+    const randomEntity =
+      entityList[Math.floor(Math.random() * entityList.length)];
+
+    return [
+      `/broadcast §6§lParry this filthy casual`,
+      `/execute at hayhayisaloser run summon minecraft:${randomEntity}`,
+    ];
+  }
+
+  randomHeight() {
+    const randomHeight = Math.floor(Math.random() * 150);
+
+    return [`/execute as HayHayIsALoser at @p run tp ~ ~${randomHeight} ~`];
+  }
+
+  giveRandomPotionEffect() {
+    const potionEffects = [
+      "minecraft:bad_omen",
+      "minecraft:blindness",
+      "minecraft:haste",
+      "minecraft:jump_boost",
+      "minecraft:levitation",
+      "minecraft:mining_fatigue",
+      "minecraft:poison",
+      "minecraft:regeneration",
+      "minecraft:resistance",
+      "minecraft:saturation",
+      "minecraft:slow_falling",
+      "minecraft:slowness",
+      "minecraft:speed",
+      "minecraft:strength",
+      "minecraft:weakness",
+      "minecraft:wither",
+    ];
+
+    const randomPotionEffect =
+      potionEffects[Math.floor(Math.random() * potionEffects.length)];
+
+    return [`/effect give @a ${randomPotionEffect} 30 1`];
+  }
 }
